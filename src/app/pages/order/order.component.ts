1519f42aeb6546d0c58396f2a1a57372b9cc2de9
valhio <valentingerasimow@gmail.com>
2023-03-19 01:57:26 +0200

    UI changes

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { StoreService } from '../../services/store.service';
 import { OrderItemStatus } from '../../enum/order-item-status.enum ';
 import { PaymentMethod } from 'src/app/enum/payment-method.enum';
+import { OrderStatus } from 'src/app/enum/order-status.enum';
 
 @Component({
   selector: 'app-order',
@@ -77,4 +78,43 @@ export class OrderComponent implements OnInit {
     return OrderItemStatus[status];
   }
 
+  getOrderStatus(status: string, date: string) {
+    const lastUpdatedOn = new Date(date).toLocaleDateString('bg-BG');
+    let response = null;
+    switch (status) {
+      case 'ORDER_PLACED':
+        response = 'Order placed';
+        break
+      case 'PENDING':
+        response = 'Preparing to ship';
+        break
+      case 'OUT_FOR_DELIVERY':
+        response = 'Shipped';
+        break
+      case 'DELIVERED':
+        response = 'Delivered';
+        break
+      case 'CANCELED':
+        response = 'Canceled';
+        break
+      case 'REFUNDED':
+        response = 'Refunded';
+        break
+      case 'RETURNED':
+        response = 'Returned';
+        break
+      default:
+        response = 'Order placed';
+        break
+    }
+    return response + ' on ' + lastUpdatedOn;
+  }
+
+  getOrderStatusIndex(status: 'ORDER_PLACED' | 'PENDING' | 'OUT_FOR_DELIVERY' | 'DELIVERED' | 'CANCELED' | 'REFUNDED' | 'RETURNED'): number {
+    return Object.keys(OrderStatus).indexOf(status);
+  }
+
+  getOrderStatusEnum(status: 'ORDER_PLACED' | 'PENDING' | 'OUT_FOR_DELIVERY' | 'DELIVERED' | 'CANCELED' | 'REFUNDED' | 'RETURNED'): string {
+    return OrderStatus[status];
+  }
 }
