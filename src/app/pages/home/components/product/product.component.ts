7918fb7101c14900f71f6a8eb92a45aca204ddb9
valhio <valentingerasimow@gmail.com>
2023-03-19 15:33:27 +0200

    Product page UI done for now

--- src/app/pages/home/components/product/product.component.ts
+++ src/app/pages/home/components/product/product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
+import { StoreService } from 'src/app/services/store.service';
+import { CartService } from '../../../../services/cart.service';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  productId: string = this.route.snapshot.params['id'];
+  product$ = this.storeService.getProductByProductId(this.productId);
+  imageOnDisplayUrl?: string = undefined;
+
+  constructor(private route: ActivatedRoute, private storeService: StoreService, private cartService: CartService) { }
 
   ngOnInit(): void {
+  }
+
+  displayImage(imageUrl: string) {
+    this.imageOnDisplayUrl = imageUrl;
+  }
+
+  onAddToCart(product: Product): void {
+    this.cartService.addToCart({
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      description: product.description,
+      imageUrl: product.imageUrl,
+      category: product.category,
+      quantity: 1,
+    });
   }
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,5 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
   ],
 }
