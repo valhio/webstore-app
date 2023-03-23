4b0554ae941e9550a66c247ca4693e5400668bfd
valhio <valentingerasimow@gmail.com>
2023-03-23 20:50:50 +0200

    Refactored code

--- src/app/pages/checkout/checkout-body/checkout-body.component.ts
+++ src/app/pages/checkout/checkout-body/checkout-body.component.ts
@@ -10,7 +10,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
 import { AuthenticationService } from '../../../services/authentication.service';
-import { Subscription } from 'rxjs';
+import { Subscription, map, tap } from 'rxjs';
 import { OnDestroy } from '@angular/core';
 import { PaymentMethod } from '../../../enum/payment-method.enum';
 
@@ -19,15 +19,26 @@ import { PaymentMethod } from '../../../enum/payment-method.enum';
   templateUrl: './checkout-body.component.html',
   styleUrls: ['./checkout-body.component.scss'],
 })
-export class CheckoutBodyComponent implements OnInit, OnDestroy {
+export class CheckoutBodyComponent implements OnDestroy {
 
   private subscriptions: Subscription[] = [];
   public paymentMethod: typeof PaymentMethod = PaymentMethod;
   orderForm: FormGroup;
-  cart: Cart = { items: [] };
-  dataSource: CartItem[] = [];
   deliveryFee: number = 7;
-PaymentMethod: any;
+  PaymentMethod: any;
+
+  cart$ = this.cartService.getCart().pipe(
+    tap((cart) => {
+      this.orderForm.patchValue({
+        productsTotal: this.getTotal(cart.items),
+        totalAmount: this.orderForm.get('productsTotal')?.value + this.deliveryFee,
+        orderItems: cart.items.map((item) => ({
+          productId: item.id, productName: item.name, quantity: item.quantity, pricePerItem: item.price,
+        })),
+      });
+    }),
+  )
+
 
   constructor(
     private cartService: CartService,
@@ -36,23 +47,22 @@ PaymentMethod: any;
     private authenticationService: AuthenticationService
   ) {
     this.orderForm = this.fb.group({
-      userId: [''],
+      userId: [null],
       firstName: ['Valentin', [Validators.required]],
       lastName: ['Gerasimov', [Validators.required]],
       email: ['booyaa323@gmail.com', [Validators.required]],
       phone: ['0876994321', [Validators.required]],
       address: ['jk. Druzhba 1, blok 48, vhod A', [Validators.required]],
       city: ['Sofia', [Validators.required]],
+      country: ['Bulgaria'],
       zipCode: ['1000', [Validators.required]],
       notes: [''],
       orderItems: [''],
       paymentMethod: [PaymentMethod.CASH_PAYMENT],
       productsTotal: [0, [Validators.required]],
       deliveryFee: [this.deliveryFee, [Validators.required]],
-      totalAmount: [
-        this.getTotal(this.dataSource) + this.deliveryFee,
-        [Validators.required],
-      ],
+      totalAmount: [[Validators.required]],
+      discount: [0, [Validators.required]],
     });
   }
 
@@ -60,45 +70,20 @@ PaymentMethod: any;
     this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 
-  ngOnInit(): void {
-    this.subscriptions.push(
-      this.cartService.cart.subscribe({
-        next: (cart) => {
-          this.cart = cart;
-          this.dataSource = cart.items;
-          let productsTotal = this.getTotal(this.dataSource);
-          this.orderForm.patchValue({
-            productsTotal: productsTotal,
-            totalAmount: productsTotal + this.deliveryFee,
-            orderItems: this.dataSource.map(item => {
-              return {
-                productId: item.id,
-                productName: item.name,
-                quantity: item.quantity,
-                pricePerItem: item.price,
-              }
-            }),
-          });
-        },
-      })
-    );
-  }
-
   getTotal(items: CartItem[]): number {
     return this.cartService.getTotal(items);
   }
 
-  onCheckout(): void {
+  onCheckout(): void { // This is for card payment
     this.cartService.checkout();
   }
 
-  onPlaceOrder(): void {
+  onPlaceOrder(): void { // This is for cash payment
     this.subscriptions.push(
       this.authenticationService.getUser().subscribe({
         next: (user) => {
           this.orderForm.patchValue({
             userId: user.userId || null,
-            discount: 0
           });
           this.cartService.placeOrder(this.orderForm.value);
         },
@@ -109,13 +94,9 @@ PaymentMethod: any;
     );
   }
 
-  notImplementedYet() {
+  notImplementedYet() { // This is for paypal payment
     this._snackBar.open('Not implemented yet', 'Close', {
       duration: 3000,
     });
-  }
-
-  addChildForm(name: string, group: FormGroup) {
-    this.orderForm.addControl(name, group);
   }
 }
