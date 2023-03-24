b3e2da54c3c1ccffe646f0d243f76b85c1a8b3cc
valhio <valentingerasimow@gmail.com>
2023-03-24 17:10:25 +0200

    Closes #18. Added passing unit tests

--- src/app/pages/home/components/invoice/invoice.component.spec.ts
+++ src/app/pages/home/components/invoice/invoice.component.spec.ts
@@ -1,23 +1,72 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InvoiceComponent } from './invoice.component';
+import { StoreService } from '../../../../services/store.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of, Observable } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('InvoiceComponent', () => {
+fdescribe('InvoiceComponent', () => {
   let component: InvoiceComponent;
   let fixture: ComponentFixture<InvoiceComponent>;
+  let storeService: StoreService;
+  let mockActivatedRoute = {
+    snapshot: {
+      params: {
+        id: '123',
+      },
+      paramMap: {
+        get(): string {
+          return '123';
+        },
+      },
+    },
+  }
+
+  const mockOrder = {
+    id: 1,
+    items: [
+      { id: 1, name: 'test', price: 100, quantity: 1 },
+      { id: 2, name: 'test2', price: 100, quantity: 1 }
+    ]
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InvoiceComponent ]
+      imports: [RouterTestingModule],
+      declarations: [InvoiceComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: StoreService, useValue: { getOrderByOrderId: () => of(mockOrder) } },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
+
+    storeService = TestBed.inject(StoreService);
     fixture = TestBed.createComponent(InvoiceComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should get order id from route params', () => {
+    expect(component.orderId).toEqual('123');
+  });
+
+  it('should fetch order data on component initialization', () => {
+    spyOn(storeService, 'getOrderByOrderId').and.returnValue(of(mockOrder));
+    component.order$ = storeService.getOrderByOrderId('123');
+
+    expect(component.order$).toBeTruthy();
+    expect(component.order$).toBeInstanceOf(Observable);
+
+    component.order$?.subscribe((order) => {
+      expect(order).toBeTruthy();
+      expect(order).toEqual(mockOrder);
+    });
+  });
 });
