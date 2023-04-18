f5a574f6098afad63d4e56c3404140425762c628
valhio <valentingerasimow@gmail.com>
2023-04-18 19:51:44 +0300

    Configured the routing for the Invoice and switched from order's id to order's orderNumber

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,50 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { CartComponent } from './pages/cart/cart.component';
-import { CheckoutComponent } from './pages/checkout/checkout.component';
-import { SuccessComponent } from './pages/payment/success/success.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { PaymentStatusComponent } from './pages/payment/payment-status/payment-status.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { AuthenticationGuard } from './guard/authentication.guard';
-import { RoleGuard } from './guard/role.guard';
-import { ManagementComponent } from './pages/management/management.component';
-import { OrdersComponent } from './pages/home/components/orders/orders.component';
-import { OrdersManagementComponent } from './pages/management/orders-management/orders-management.component';
-import { OrderComponent } from './pages/order/order.component';
-import { InvoiceComponent } from './pages/home/components/invoice/invoice.component';
-import { ProductComponent } from './pages/home/components/product/product.component';
-import { AccountComponent } from './pages/account/account.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {CartComponent} from './pages/cart/cart.component';
+import {CheckoutComponent} from './pages/checkout/checkout.component';
+import {SuccessComponent} from './pages/payment/success/success.component';
+import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
+import {PaymentStatusComponent} from './pages/payment/payment-status/payment-status.component';
+import {LoginComponent} from './pages/login/login.component';
+import {RegisterComponent} from './pages/register/register.component';
+import {AuthenticationGuard} from './guard/authentication.guard';
+import {RoleGuard} from './guard/role.guard';
+import {ManagementComponent} from './pages/management/management.component';
+import {OrdersComponent} from './pages/home/components/orders/orders.component';
+import {OrdersManagementComponent} from './pages/management/orders-management/orders-management.component';
+import {OrderComponent} from './pages/order/order.component';
+import {InvoiceComponent} from './pages/home/components/invoice/invoice.component';
+import {ProductComponent} from './pages/home/components/product/product.component';
+import {AccountComponent} from './pages/account/account.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', redirectTo: '', pathMatch: 'full' },
-  { path: 'management', component: ManagementComponent, canActivate: [AuthenticationGuard, RoleGuard], data: { expectedRoles: ['ROLE_SUPER_ADMIN'] } },
-  { path: 'cart', component: CartComponent },
-  { path: 'checkout', component: CheckoutComponent },
-  { path: 'orders', component: OrdersComponent, canActivate: [AuthenticationGuard] },
-  { path: 'orders/:id', component: OrderComponent, canActivate: [AuthenticationGuard] },
-  { path: 'orders/:id/invoice', component: InvoiceComponent, canActivate: [AuthenticationGuard] },
-  { path: 'products/:id', component: ProductComponent },
-  { path: 'management/orders', component: OrdersManagementComponent, canActivate: [AuthenticationGuard, RoleGuard], data: { expectedRoles: ['ROLE_SUPER_ADMIN', 'ROLE_MANAGER'], } },
-  { path: 'payment/success', component: SuccessComponent },
-  { path: 'payment/status', component: PaymentStatusComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'user/myaccount', component: AccountComponent },
-  { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
+  {path: '', component: HomeComponent},
+  {path: 'home', redirectTo: '', pathMatch: 'full'},
+  {
+    path: 'management',
+    component: ManagementComponent,
+    canActivate: [AuthenticationGuard, RoleGuard],
+    data: {expectedRoles: ['ROLE_SUPER_ADMIN']}
+  },
+  {path: 'cart', component: CartComponent},
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'orders', component: OrdersComponent, canActivate: [AuthenticationGuard]},
+  {path: 'orders/:orderNumber', component: OrderComponent, canActivate: [AuthenticationGuard]},
+  {path: 'orders/:orderNumber/invoice/:invoiceNumber', component: InvoiceComponent, canActivate: [AuthenticationGuard]},
+  {path: 'products/:id', component: ProductComponent},
+  {
+    path: 'management/orders',
+    component: OrdersManagementComponent,
+    canActivate: [AuthenticationGuard, RoleGuard],
+    data: {expectedRoles: ['ROLE_SUPER_ADMIN', 'ROLE_MANAGER'],}
+  },
+  {path: 'payment/success', component: SuccessComponent},
+  {path: 'payment/status', component: PaymentStatusComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'user/myaccount', component: AccountComponent},
+  {path: '**', pathMatch: 'full', component: PageNotFoundComponent},
 ];
 
 @NgModule({
