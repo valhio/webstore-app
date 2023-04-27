600ef8541a37b5e1929830e8d3fd56e9e41f1579
valhio <valentingerasimow@gmail.com>
2023-04-27 12:49:54 +0300

    Moved the displaying of reviews logic into a separate component

--- src/app/pages/home/components/product/product-review/product-review.component.ts
+++ src/app/pages/home/components/product/product-review/product-review.component.ts
@@ -16,65 +16,7 @@ export class ProductReviewComponent {
   @Input() productId: string | undefined;
   @Output() newProductReview = new EventEmitter<any[]>();
 
-  subscriptions: Subscription[] = [];
-
-  constructor(private authService: AuthenticationService, private storeService: StoreService, private router: Router) { }
-
-  commentForm = new FormGroup({
-    comment: new FormControl('', Validators.required),
-  })
-
-  toggleCommentForm(productReview: any): void {
-    if (!this.authService.isAuthenticated()) this.router.navigate(['/login']);
-    this.commentForm.reset();
-    productReview.isCommentFormVisible = !productReview.isCommentFormVisible;
-  }
-
-  toggleCommentsForReview(productReview: any): void {
-    productReview.areCommentsVisible = !productReview.areCommentsVisible;
-  }
-
-  onLikeReview(reviewId: number): void {
-    const isLiked$ = this.storeService.hasUserLikedReview(reviewId); // Check if the user has liked the review
-    const likeAction$ = isLiked$.pipe(
-      switchMap(hasUserLikedReview => {
-        // If the user has liked the review, unlike it, otherwise like it
-        const likeAction = hasUserLikedReview ? this.storeService.unlikeReview(reviewId) : this.storeService.likeReview(reviewId);
-        return likeAction.pipe( // Return the review likes observable
-          switchMap(() => this.storeService.getReviewLikes(reviewId))
-        );
-      })
-    );
-    this.subscriptions.push(
-      likeAction$.subscribe(reviewLikes => {
-        const productReview = this.productReviews.find((pr: any) => pr.id === reviewId);
-        if (productReview) {
-          productReview.likes = reviewLikes;
-        }
-      })
-    );
-  }
-
-  hasUserLikedReview(review: any) {
-    return review.likes.find((like: any) => like.user.userId == this.authService.getUserUserId()) != undefined;
-  }
-
-  submitComment(productReviewId: string): void {
-    if (this.commentForm.valid) {
-      this.subscriptions.push(
-        this.storeService.addCommentToReview(productReviewId, this.commentForm.value['comment']!).subscribe((comment) => {
-          this.commentForm.reset();
-
-          // Find the review to which the comment was added and add the comment to the review's comments array, so that the comment is displayed in the UI
-          this.productReviews.forEach((productReview: any) => {
-            if (productReview.id == productReviewId) {
-              productReview.comments.push(comment);
-            }
-          })
-        })
-      )
-    }
-  }
+  constructor() { }
 
   onNewProductReviewSubmitted(productReviews: any[]): void {
     this.productReviews = productReviews;
